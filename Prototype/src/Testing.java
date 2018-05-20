07cbc36baf63ceeb031132be61a96ab84ad65f5a
Armando Jaramillo <ajaramillo18@hotmail.com>
2018-05-19 22:06:09 -0500

    first and finished

---
+++ Prototype/src/Testing.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+/**
+ * @author ajara
+ *
+ */
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Voter amalia = new Voter();
+		amalia.setAge(25);
+		amalia.setName("Amalia Solorzano");
+		Voter tonia = null;
+		try {
+			tonia = (Voter) amalia.clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		tonia.setAge(30);
+		
+		System.out.println("Amalia edad: "+ amalia.getAge());
+		System.out.println("Amalia nombre: "+ amalia.getName());
+		System.out.println("Tonia edad: "+ tonia.getAge());
+		System.out.println("Tonia nombre : "+ tonia.getName());
+
+	}
+
+}
---
+++ Prototype/src/Voter.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+/**
+ * @author ajara
+ *
+ */
+public class Voter implements Cloneable{
+
+	private String name;
+	private int age;
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+		
+	
+}
