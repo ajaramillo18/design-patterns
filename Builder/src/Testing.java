d5e792ee984ec83b24f6a0f946ce5a3dffd2972c
Armando Jaramillo <ajaramillo18@hotmail.com>
2018-05-26 07:06:50 -0500

    format code

--- Builder/src/Testing.java
+++ Builder/src/Testing.java
@@ -13,28 +13,18 @@ public class Testing {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		
 
 		try {
-			
-	
-			Siniestro sin = new Siniestro.SiniestroBuilder()
-					.delincuente("EPN")
-					.aseguradora("Sup0er")
-					.build();
-			
-		}
-		catch (IllegalStateException e) {
-			
-			System.out.println("ERROR:" + e.getMessage() );
+
+			Siniestro sin = new Siniestro.SiniestroBuilder().delincuente("EPN").aseguradora("Sup0er").build();
+
+		} catch (IllegalStateException e) {
+
+			System.out.println("ERROR:" + e.getMessage());
 			e.printStackTrace();
 		}
-		
-		
-		//siniestro en oxxo
-		
-		
+
+		// siniestro en oxxo
 
 	}
 
